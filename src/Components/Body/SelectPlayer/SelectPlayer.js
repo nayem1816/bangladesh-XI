e9c640b662f03741e3a1fc929ccab15478d8af71
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-03-01 15:14:25 +0600

    some change

--- src/Components/Body/SelectPlayer/SelectPlayer.js
+++ src/Components/Body/SelectPlayer/SelectPlayer.js
@@ -6,15 +6,12 @@ import {faDollarSign} from '@fortawesome/free-solid-svg-icons'
 const SelectPlayer = (props) => {
     const countPlayer = props.countPlayer;
     let names = [];
+    let totalSalary = 0;
     for (let i = 0; i < countPlayer.length; i++) {
         const player = countPlayer[i];
         const playerName = player.name;
         const salary = player.salary;
         names = names + playerName +" ($" + salary + ")"+ ", ";
-    }
-    let totalSalary = 0;
-    for (let i = 0; i < countPlayer.length; i++) {
-        const player = countPlayer[i];
         const numSalary = parseInt(player.salary);
         totalSalary = totalSalary + numSalary;
     }
