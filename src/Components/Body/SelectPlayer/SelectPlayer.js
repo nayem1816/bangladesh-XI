a3368c61eb4020244f6d86cc80d0666db2fa072c
Md Nayem Hossain <2191081070@uttarauniversity.edu.bd>
2021-03-01 15:52:40 +0600

    some change2

--- src/Components/Body/SelectPlayer/SelectPlayer.js
+++ src/Components/Body/SelectPlayer/SelectPlayer.js
@@ -20,8 +20,8 @@ const SelectPlayer = (props) => {
             <h2>Selected Player</h2>
             <div className="select-player-body">
                 <h4 className="total-select-player">Total Select Player: {countPlayer.length}</h4>
-                <h4 className="select-player-name">Selected Player Name: <small>{names}</small></h4>
-                <h4 className="total-salary">Total Salary: <FontAwesomeIcon icon={faDollarSign} />{totalSalary}</h4>
+                <h4 className="select-player-name">Selected Player Name & Salary: <small>{names}</small></h4>
+                <h4 className="total-salary">Total Budget: <FontAwesomeIcon icon={faDollarSign} />{totalSalary}</h4>
             </div>
         </div>
     );
